d2da70739371fe9dc95acf5f2706fa7eea92bc5b
saji2000 <sajaddaneshmand79@gmail.com>
2025-10-08 23:49:07 -0400

    Working on login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,11 +9,28 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
+    public function logout(){
+        auth()->logout();
+        return redirect('/');
+    }
+
+    public function login(Request $request){
+        $incomingFields = $request->validate([
+            'loginname' => ['required', 'min:3', 'max:50'],
+            'loginpassword' => ['required', 'string', 'min:3', 'max:30']
+        ]);
+
+        if (auth()->attempt(['name' => $incomingFields['loginname'], 'password' => $incomingFields['loginpassword']])){
+            $request->session()->regenerate();
+        }
+        return redirect('/');
+    }
+
     public function register(Request $request){
         $incomingFields = $request->validate([
             'name' => ['required', 'min:3', 'max:50', Rule::unique('users', 'name')],
             'email' => ['required', 'email', 'max:80', Rule::unique('users', 'email')],
-            'password' => ['required', 'string', 'min:8', 'max:30']
+            'password' => ['required', 'string', 'min:3', 'max:30']
         ]);
 
         $incomingFields['password'] = bcrypt($incomingFields['password']);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,13 @@
 </head>
 <body>
     @auth
-    <p>Welcome back {{auth()->user()->name}}!</p>
+    <p>Congrats you are logged in {{auth()->user()->name}}!</p>
+    <form action="/logout" method="POST">
+        @csrf
+        <button>Log Out</button>
+    </form>
     @else
     <p>Please register or log in.</p>
-    @endauth
     <div style="border: 3px solid black;">
         <h2>Home Page</h2>
         <form action="/register" method="POST">
@@ -19,5 +22,15 @@
         <input name="password" type="password" placeholder="password">
         <button type="submit">Submit</button>
     </div>
+
+    <div style="border: 3px solid black;">
+        <h2>Login</h2>
+        <form action="/login" method="POST">
+        @csrf
+        <input name="loginname" type="text" placeholder="name">
+        <input name="loginpassword" type="password" placeholder="password">
+        <button type="submit">Submit</button>
+    </div>
+    @endauth
 </body>
 </html>
